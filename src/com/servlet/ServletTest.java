be5856c45533639e12c3524d9d057a2df6e9cfd7
Weichun Wang <seven.wangweichun@outlook.com>
2023-08-30 12:13:42 +1200

    upload

--- src/com/servlet/ServletTest.java
+++ src/com/servlet/ServletTest.java
@@ -47,10 +47,10 @@ public class ServletTest extends HttpServlet {
 		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		String msg = (String) request.getParameter("msg");
-		String sign = (String) request.getParameter("sign");
+		String msg = (String) request.getParameter("msg"); 
+		String sign = (String) request.getParameter("sign"); 
 		String publicKeyString = (String)request.getParameter("publicKey");
-		//String publicKeyString = "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHO8lxrizZisNpk6UFlcBk5wXD0AQcOOEDgCKtWo60oYHgU5WtNV7IxwGnDqDlGEoSEGYnpgWxljmVI/Jhfs9UK1jjOkZHgs6ajJffNLitC/E0EOqQ/WBimJF0RV0yWpvGzSXmc1J151xhag4pxK+QK1j+RNGbHLVeS7vkwuk+CvAgMBAAE=";
+		///String publicKeyString = "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHO8lxrizZisNpk6UFlcBk5wXD0AQcOOEDgCKtWo60oYHgU5WtNV7IxwGnDqDlGEoSEGYnpgWxljmVI/Jhfs9UK1jjOkZHgs6ajJffNLitC/E0EOqQ/WBimJF0RV0yWpvGzSXmc1J151xhag4pxK+QK1j+RNGbHLVeS7vkwuk+CvAgMBAAE=";
 		
 		System.out.println("msg = " + msg);
 		System.out.println("sign = " + sign);
